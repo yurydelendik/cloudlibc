7901135f627b994ee9a78f15fae5fcc291b5ac8c
Yury Delendik <ydelendik@mozilla.com>
2018-12-07 10:46:38 -0500

    Cloudabi dependency

--- wasm.mk
+++ wasm.mk
@@ -1,12 +1,15 @@
 WC = $(HOME)/Work/wasmception
-CABI = ./cloudabi
+CABI = ./contrib/cloudabi
 
 default:
 	CC=$(WC)/dist/bin/clang AR=$(WC)/dist/bin/llvm-ar \
-	CFLAGS="--target=wasm32-unknown-unknown-wasm --sysroot=$(WC)/sysroot -D__wasm__ -D__CloudABI__ -DCRT1 -I$(CABI)/headers -g" \
+	CFLAGS="--target=wasm32-unknown-unknown-wasm --sysroot=$(WC)/sysroot -D__wasm__ -D__CloudABI__ -DCRT1 -I$(CABI)/headers -g -O2" \
 	./build notests
 
-install:
+install: default
 	PREFIX=`pwd`/_sysroot ./install notests
 
-.PHONY: default install
+clean:
+	rm -rf _sysroot _obj
+
+.PHONY: default install clean
